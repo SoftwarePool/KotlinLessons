7a469199fd0a5055d4a090e14ad8b711185f05e3
nisaefendioglu <nisaefendioglu0@gmail.com>
2022-06-30 17:15:01 +0300

    edited folders

--- src/main/kotlin/1 Getting Started/HelloWorld.kt
+++
@@ -1,5 +0,0 @@
-package `1 Getting Started`
-
-fun main() {
-    println("Hello World!")
-}
--- src/main/kotlin/1 Getting Started/Variables.kt
+++
@@ -1,9 +0,0 @@
-package `1 Getting Started`
-
-fun main() {
-    //value immutable
-    //variable mutable
-    
-    val name : String = "John"
-    var age : Int = 20
-}
